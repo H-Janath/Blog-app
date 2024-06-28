b3b547a58002e0d7e9f08ef8dbb15bb4db78c6ad
H-Janath <janathhma@gmail.com>
2024-06-28 16:25:36 +0530

    Creat get post API route

--- api/route/post.route.js
+++ api/route/post.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { create } from '../controller/post.controller.js';
+import { create, getPost } from '../controller/post.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
 const route = express.Router();
 
 route.post("/create",verifyToken,create);
+route.get('/getPost',getPost);
 
 export default route;
